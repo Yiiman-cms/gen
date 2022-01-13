d2700158b01a729f65ed2fe22c4665a1d74948f2
غلامرضا بهشتیان <amintado@gmail.com>
2022-01-13 20:50:41 +0330

    solve directory address problems

--- src/GiiAsset.php
+++ src/GiiAsset.php
@@ -17,7 +17,7 @@ use yii\web\AssetBundle;
  */
 class GiiAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/yiiman/yii-module-gen/assets';
+    public $sourcePath = '@vendor/yiiman/yii-module-gen/src/assets';
     public $css = [
         'main.css',
         'css/fontiran.css',
